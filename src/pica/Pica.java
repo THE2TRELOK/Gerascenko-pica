98de9f60a872d07da0986443d22df043c1dbe08e
Dmitrijs <ggamersshd@gmail.com>
2022-01-31 23:01:45 +0200

    Uztaisita klase pica

--- src/pica/Pica.java
+++ src/pica/Pica.java
@@ -1,5 +1,20 @@
 package pica;
 
-public class Pica {
+import javax.swing.JOptionPane;
 
+public class Pica {
+int izmers;
+String piedavas;
+String dzeriens;
+Double cena1;
+Pica(int cm,String nacinka,String dzeriens,Double cena1){
+	this.izmers=cm;
+	this.piedavas=nacinka;
+	this.dzeriens=dzeriens;
+	this.cena1=cena1;
 }
+void izvadit(){
+	JOptionPane.showMessageDialog(null,"Jusu picas izmers- "+izmers+"\nJusu pica- "+piedavas+"\nDzeriens- "+dzeriens+"\nJusu pasutijuma summa "+cena1+"ˆ");
+		
+}
+	}
