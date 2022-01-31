9cbdab281fc83a22e340009a5f20e3848e559db0
Dmitrijs <ggamersshd@gmail.com>
2022-01-31 23:11:17 +0200

    Uzsakta metode uztaisit picu

---
+++ src/pica/Picerija.java
@@ -0,0 +1,29 @@
+package pica;
+
+import javax.swing.JOptionPane;
+
+public class Picerija {
+	static  Pica[] izveidotPicu( Pica[]masivs) {
+		double cena1=0;
+		int izmers=0;
+		String piedavas,dzeriens;
+			for(int i=0;i<masivs.length;i++) {
+			    
+			    	izmers = Integer.parseInt(JOptionPane.showInputDialog("Izvelies picas izmeru,20,30,40)"));
+					
+				piedavas = JOptionPane.showInputDialog("Kadu picu velaties ir: (peperoni, studentu, havaju, amerikanu)");
+				piedavas = piedavas.toLowerCase();
+				
+				dzeriens = JOptionPane.showInputDialog("Izvelejaties dzerienu: cola , fanta, sprite");
+			    
+				masivs[i]= new Pica(izmers,piedavas,dzeriens,cena1);
+			}
+			return masivs;
+	}
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
